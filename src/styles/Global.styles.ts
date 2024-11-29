2d2b413350d389c96a5decedd3fd05f9b3ca197b
gabriel <gabrielminima@gmail.com>
2024-11-29 13:57:30 -0300

    created Global Styles

---
+++ src/styles/Global.styles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        font-family: "Rubik", serif;
+        background-color: ${({ theme }) => theme.colors.background};
+        color: ${({ theme }) => theme.colors.text};
+        
+    }
+
+    input, button{
+        font-family: inherit;
+        border: none;
+    }
+
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    colors: {
+      background: "#F5F7FA",
+      primary: "#2B2BFF",
+      text: "#2C2C2C",
+      cardBackground: "#FFFFFF",
+      border: "#E0E4E8",
+    },
+    fonts: {
+      title: "700 24px 'Roboto', sans-serif",
+      text: "400 16px 'Roboto', sans-serif",
+      semibold: "600 16px 'Roboto', sans-serif",
+    },
+  };
+  
